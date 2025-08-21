aa54d8493bc83aa665597ef46d10d18fc4ae7149
suhail ahmed malik <muhmmadasifmalik593@gmail.com>
2025-08-21 09:13:57 +0530

    add Selection sort algo

---
+++ SelectionSort.java
@@ -0,0 +1,29 @@
+package algorithmsandproblems;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int []s={4,3,2,9,1,7,5,6,8};
+        selectionSort(s);
+        for (int y:s){
+            System.out.print(y+" ");
+        }
+    }
+    public static void selectionSort(int[]a){
+        int n=a.length;
+        for (int i=0;i<n-1;i++){
+            int min=i;
+            for (int j=i+1;j<n;j++){
+                if (a[j]<a[min]){
+                    min=j;
+
+                }
+            }
+
+            int temp=a[min];
+            a[min]=a[i];
+            a[i]=temp;
+
+        }
+
+    }
+}
