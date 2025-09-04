be4bf466628f29e5c00747f2bf4a2af20b080f6b
suhail ahmed malik <muhmmadasifmalik593@gmail.com>
2025-09-04 16:38:26 +0530

    adding n no of fibnocii series elements in an array

---
+++ Program15.java
@@ -0,0 +1,29 @@
+package algorithmsandproblems;
+//WAJP to store n terms of Fibonacci series into
+//an array.
+
+import java.util.Scanner;
+
+public class Program15 {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int []b=storeNFibonacciNumbers(n);
+        for (int L:b){
+            System.out.print(L+" ");
+        }
+
+    }
+    public static int []storeNFibonacciNumbers(int n){
+        int []a=new int [n];
+        if (n==0)return a;
+        a[0]=0;
+        if ( n==1)return a;
+        a[1]=1;
+        for (int i=2;i<n;i++){
+            a[i]=a[i-1]+a[i-2];
+
+        }
+        return a;
+    }
+}
